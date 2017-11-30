b05098a82aa91017641aa3c691ad4c0632e1bf14
OlhaPolishchuk <olapol6@gmail.com>
2017-11-30 12:15:11 +0200

    updated according to lab work 9

--- js/news.js
+++ js/news.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
      function updateOnlineStatus(event) {
-         if(isOnline()){
+         if(isOnline()) {
              readOfflineNews();
          }
      }
@@ -12,7 +12,10 @@ window.addEventListener('load', function() {
      return window.navigator.onLine;
  }
 
+
  function readOfflineNews() {
+   if (useLocalStorage) {
+        len = localStorage.length + 1;
      len = localStorage.length + 1;
      for (var k = 1; k < len; k++){
          news = JSON.parse(localStorage.getItem('n'+k));
@@ -29,5 +32,26 @@ window.addEventListener('load', function() {
              "</a></div></div>";
          parentElem.appendChild(out);
          localStorage.removeItem(k);
+       }
+       } else {
+        var transaction = db.transaction(["news"], "readonly");
+        var store = transaction.objectStore("news");
+        store.openCursor().onsuccess = function (e) {
+            var cursor = e.target.result;
+            if (cursor) {
+                cursor.continue();
+                var parentElem = document.getElementById('news-list');
+                var out = document.createElement('div');
+                out.id = 'news';
+                out.innerHTML =
+                    "<div class='col-md-4'>" +
+                    "<div class='card'> " +
+                    "<img src='" + cursor.value.img + "' width='100%'>" +
+                    "<div class='caption'><h2>" + cursor.value.name + "</h2></div>" +
+                    " <p>" + cursor.value.text + "</p>" +
+                    "</a></div></div>";
+                parentElem.appendChild(out);
+            }
+        }
      }
  }
